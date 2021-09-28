3ee54f60e35f7f2739b805159335a47afc5c96e0
jamesate4e <james.chinwokwu@e4email.net>
2021-09-28 01:20:32 +0100

    docs: add documentation the code

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const { filter, matches } = require("lodash");
 const EXTERNAL_URL =
   "https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json";
 
+/*
+ * This searches the external json file and returns the data
+ * For various search criteria in accordanace with the request result object
+ *
+ * example:
+ * you can search for the clinics by name and StateName
+ * know the EXTERNEL API result object structure is something like this
+ * [
+ *  {
+ *   "name":"Scratchpay Official practice",
+ *   "stateName":"Tennessee",
+ *   "availability":{
+ *      "from":"00:00","to":"24:00"
+ *     }
+ *   }
+ * ]
+ * then we can have something like this
+ * GET /clinics?name="Scratchpay Official practice"&stateName=Tennessee
+ */
 app.get("/", async (req, res) => {
   try {
     const { data } = await axios.get(EXTERNAL_URL);
